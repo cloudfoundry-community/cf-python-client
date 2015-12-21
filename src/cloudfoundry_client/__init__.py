9c9ef74aeb09acff9ef32054d65741f027ffd729
Ben Einaudi <benjamin.einaudi@orange.com>
2015-12-21 14:24:31 +0100

    Prepare 0.0.5

--- src/cloudfoundry_client/__init__.py
+++ src/cloudfoundry_client/__init__.py
@@ -3,7 +3,7 @@
     This module provides a client library for cloudfoundry_client v2.
 """
 
-__version__ = "0.0.4"
+__version__ = "0.0.5"
 
 from main import main, build_client_from_configuration
 from client import CloudFoundryClient
