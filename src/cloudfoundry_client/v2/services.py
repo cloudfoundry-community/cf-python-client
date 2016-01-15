f009b7d06c9c647181dbd8679570624e81747bee
Ben Einaudi <benjamin.einaudi@orange.com>
2016-01-15 14:39:46 +0100

    Add api for service brokers

--- src/cloudfoundry_client/v2/services.py
+++ src/cloudfoundry_client/v2/services.py
@@ -47,5 +47,26 @@ class ServiceBindingManager(EntityManager):
         super(ServiceBindingManager, self)._remove(binding_id)
 
 
+class ServiceBrokerManager(EntityManager):
+    def __init__(self, target_endpoint, credentials_manager):
+        super(ServiceBrokerManager, self).__init__(target_endpoint, credentials_manager, '/v2/service_brokers')
 
+    def create(self, broker_url, broker_name, auth_username, auth_password, space_guid=None):
+        request = dict(broker_url=broker_url,
+                       name=broker_name,
+                       auth_username=auth_username,
+                       auth_password=auth_password)
+        if space_guid is not None:
+            request['space_guid'] = space_guid
+        return super(ServiceBrokerManager, self)._create(request)
+
+    def update(self, broker_guid, broker_url, broker_name, auth_username, auth_password):
+        request = dict(broker_url=broker_url,
+                       name=broker_name,
+                       auth_username=auth_username,
+                       auth_password=auth_password)
+        return super(ServiceBrokerManager, self)._update(broker_guid, request)
+
+    def remove(self, broker_guid):
+        super(ServiceBrokerManager, self)._remove(broker_guid)
 
