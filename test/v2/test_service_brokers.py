b8f2cdeb11002213831943915af710c5a506799a
Ben Einaudi <benjamin.einaudi@orange.com>
2016-01-21 13:02:40 +0100

    Add some tests

---
+++ test/v2/test_service_brokers.py
@@ -0,0 +1,23 @@
+from config_test import build_client_from_configuration
+import unittest
+import logging
+import json
+
+_logger = logging.getLogger(__name__)
+
+
+
+class TestServiceBrokers(unittest.TestCase):
+
+    def test_list(self):
+        cpt = 0
+        client = build_client_from_configuration()
+        for broker in client.service_broker.list():
+            if cpt == 0:
+                self.assertIsNotNone(
+                    client.service_broker.get_first(space_guid=broker['entity']['space_guid']))
+                self.assertIsNotNone(
+                    client.service_instance.get(broker['metadata']['guid']))
+            cpt += 1
+            _logger.debug(broker)
+        _logger.debug('test broker list - %d found', cpt)
--- test/v2/test_service_plans.py
+++ test/v2/test_service_plans.py
@@ -11,12 +11,12 @@ class TestServicePlan(unittest.TestCase):
     def test_list_instance_for_plan(self):
         client = build_client_from_configuration()
         for instance in client.service_plan.list_instance(client.plan_guid, space_guid=client.space_guid):
-            _logger.debug('test instance list - %s -%s', instance['metadata']['guid'], instance['entity']['name'])
+            _logger.debug('test_list_instance_for_plan - %s -%s', instance['metadata']['guid'], instance['entity']['name'])
 
     def test_list(self):
         cpt = 0
         client = build_client_from_configuration()
-        for plan in client.service_plan.list(service_guid=client.service_guid):
+        for plan in client.service_plan.list(service_broker_guid=client.service_guid):
             if cpt == 0:
                 _logger.debug(json.dumps(plan))
             cpt += 1
