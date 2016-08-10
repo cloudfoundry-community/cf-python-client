95bbd25e3b40790c7bf892d0a9f591a705384cf2
Ben Einaudi <benjamin.einaudi@orange.com>
2016-08-10 15:24:28 +0200

    Prepare next version

--- src/python/cloudfoundry_client/__init__.py
+++ src/python/cloudfoundry_client/__init__.py
@@ -3,7 +3,7 @@
     This module provides a client library for cloudfoundry_client v2.
 """
 
-__version__ = "0.0.12"
+__version__ = "0.0.13"
 
 from main import main, build_client_from_configuration
 from client import CloudFoundryClient
