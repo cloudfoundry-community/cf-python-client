cloudfoundry-community/cf-python-client

commit 5e5d45e74f3263e5314514d1cc387ac8213845b6

make `python setup.py test` work

setup.py
@@ -8,16 +8,16 @@ src_dir = 'src/python'
 package_directory = 'cloudfoundry_client'
 package_name = 'cloudfoundry-client'
 loggregator_dir = 'loggregator'
+sys.path.insert(0, os.path.realpath(src_dir))
 
-__version__ = None
 version_file = '%s/%s/__init__.py' % (src_dir, package_directory)
 with open(version_file, 'r') as f:
     for line in f.readlines():
         if line.find('__version__') >= 0:
             exec line
-
-if __version__ is None:
-    raise AssertionError('Failed to load version from %s' % version_file)
+            break
+    else:
+        raise AssertionError('Failed to load version from %s' % version_file)
 
 
 def purge_sub_dir(path):
@@ -66,5 +66,7 @@ setup(name=package_name,
       },
       cmdclass=dict(generate=GenerateCommand),
       package_dir={package_directory: '%s/%s' % (src_dir, package_directory)},
-      install_requires=[requirement.rstrip(' \r\n') for requirement in open('requirements.txt').readlines()],
+      install_requires=[requirement.rstrip(' \r\n') for requirement in open('requirements.txt')],
+      tests_require=[line.rstrip() for line in open('test/requirements.txt')],
+      test_suite='test',
       )

test/python/__init__.py
@@ -1,0 +1,10 @@
+#!/usr/bin/env python
+
+import os
+import sys
+import unittest
+
+def get_suite():
+    loader = unittest.TestLoader()
+    suite = loader.loadTestsFromModule(__package__)
+    return suite

commit f908460fa6a843a0989c9ca7c0b5f446860a3319

fix #7: pip install broken by 5e5d45e

setup.py
@@ -68,6 +68,8 @@ setup(name=package_name,
       cmdclass=dict(generate=GenerateCommand),
       package_dir={package_directory: '%s/%s' % (src_dir, package_directory)},
       install_requires=[requirement.rstrip(' \r\n') for requirement in open('requirements.txt')],
-      tests_require=[line.rstrip() for line in open('test/requirements.txt')],
+      tests_require=[
+          'mock==2.0.0',
+      ],
       test_suite='test',
       )