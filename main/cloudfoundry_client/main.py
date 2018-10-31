25f89a72505380b2632b7c7d94612cebcc16d89e
Ben Einaudi <benjamin.einaudi@orange.com>
2018-10-31 08:29:28 +0100

    Add import_from_cli command to copy token

--- main/cloudfoundry_client/main.py
+++ main/cloudfoundry_client/main.py
@@ -34,13 +34,37 @@ def _read_value_from_user(prompt, error_message=None, validator=None, default=''
                 sys.stderr.write('\"%s\": %s\n' % (answer_value, error_message))
 
 
-def build_client_from_configuration(previous_configuration=None):
+def get_user_directory():
     dir_conf = os.path.join(os.path.expanduser('~'))
     if not os.path.isdir(dir_conf):
         if os.path.exists(dir_conf):
             raise IOError('%s exists but is not a directory')
         os.mkdir(dir_conf)
-    config_file = os.path.join(dir_conf, '.cf_client_python.json')
+    return dir_conf
+
+def get_config_file():
+    return os.path.join(get_user_directory(), '.cf_client_python.json')
+
+
+def import_from_clf_cli():
+    user_directory = get_user_directory()
+    cf_cli_dir = os.path.join(user_directory, '.cf')
+    if not os.path.isdir(cf_cli_dir):
+        raise IOError('%s directory not found' % cf_cli_dir)
+    config_file = os.path.join(cf_cli_dir, 'config.json')
+    if not os.path.isfile(config_file):
+        raise IOError('%s not found' % config_file)
+    with open(config_file, 'r') as cf_cli_file:
+        cf_cli_data = json.load(cf_cli_file)
+        if cf_cli_data['RefreshToken'] is None or cf_cli_data['Target'] is None:
+            raise IOError('Could not load informations from cf cli configuration')
+        with open(get_config_file(), 'w') as f:
+            f.write(json.dumps(dict(target_endpoint=cf_cli_data['Target'],
+                                    skip_ssl_verification=True,
+                                    refresh_token=cf_cli_data['RefreshToken']), indent=2))
+
+def build_client_from_configuration(previous_configuration=None):
+    config_file = get_config_file()
     if not os.path.isfile(config_file):
         target_endpoint = _read_value_from_user('Please enter a target endpoint',
                                                 'Url must starts with http:// or https://',
@@ -78,6 +102,9 @@ def build_client_from_configuration(previous_configuration=None):
                 _logger.exception("Could not restore configuration. Cleaning and recreating")
                 os.remove(config_file)
                 return build_client_from_configuration(configuration)
+
+
+
 
 
 def is_guid(s):
@@ -174,6 +201,7 @@ def main():
     parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter)
     parser.add_argument('-V', '--version', action='version', version=__version__)
     subparsers = parser.add_subparsers(title='Commands', dest='action', description='\n'.join(description))
+    subparsers.add_parser('import_from_cf_cli', help='Copy CF CLI configuration into our configuration')
     for domain, command_description in list(commands.items()):
         list_parser = subparsers.add_parser('list_%ss' % domain)
         for filter_parameter in command_description['list']:
@@ -203,68 +231,72 @@ def main():
                                             help='The id. Can be UUID or name (first found then)')
 
     arguments = parser.parse_args()
-    client = build_client_from_configuration()
-    if arguments.action == 'recent_logs':
-        resource_id = resolve_id(arguments.id[0], lambda x: client.apps.get_first(name=x), 'application', True)
-        log_recent(client, resource_id)
-    elif application_commands.get(arguments.action) is not None:
-        resource_id = resolve_id(arguments.id[0], lambda x: client.apps.get_first(name=x), 'application', True)
-        print(getattr(client.apps, application_commands[arguments.action][0])(resource_id).json(indent=1))
-    elif application_extra_list_commands.get(arguments.action) is not None:
-        resource_id = resolve_id(arguments.id[0], lambda x: client.apps.get_first(name=x), 'application', True)
-        name_property = application_extra_list_commands[arguments.action][2]
-        for entity in getattr(client.apps, application_extra_list_commands[arguments.action][0])(resource_id):
-            print('%s - %s' % (entity['metadata']['guid'], entity['entity'][name_property]))
-    elif arguments.action.find('list_') == 0:
-        domain = arguments.action[len('list_'): len(arguments.action) - 1]
-        filter_list = dict()
-        for filter_parameter in commands[domain]['list']:
-            filter_value = getattr(arguments, filter_parameter)
-            if filter_value is not None:
-                filter_list[filter_parameter] = filter_value
-        for entity in _get_client_domain(client, domain).list(**filter_list):
-            name_property = commands[domain]['name']
-            if name_property is not None:
+    if arguments.action == 'import_from_cf_cli':
+        import_from_clf_cli()
+    else:
+        client = build_client_from_configuration()
+
+        if arguments.action == 'recent_logs':
+            resource_id = resolve_id(arguments.id[0], lambda x: client.apps.get_first(name=x), 'application', True)
+            log_recent(client, resource_id)
+        elif application_commands.get(arguments.action) is not None:
+            resource_id = resolve_id(arguments.id[0], lambda x: client.apps.get_first(name=x), 'application', True)
+            print(getattr(client.apps, application_commands[arguments.action][0])(resource_id).json(indent=1))
+        elif application_extra_list_commands.get(arguments.action) is not None:
+            resource_id = resolve_id(arguments.id[0], lambda x: client.apps.get_first(name=x), 'application', True)
+            name_property = application_extra_list_commands[arguments.action][2]
+            for entity in getattr(client.apps, application_extra_list_commands[arguments.action][0])(resource_id):
                 print('%s - %s' % (entity['metadata']['guid'], entity['entity'][name_property]))
+        elif arguments.action.find('list_') == 0:
+            domain = arguments.action[len('list_'): len(arguments.action) - 1]
+            filter_list = dict()
+            for filter_parameter in commands[domain]['list']:
+                filter_value = getattr(arguments, filter_parameter)
+                if filter_value is not None:
+                    filter_list[filter_parameter] = filter_value
+            for entity in _get_client_domain(client, domain).list(**filter_list):
+                name_property = commands[domain]['name']
+                if name_property is not None:
+                    print('%s - %s' % (entity['metadata']['guid'], entity['entity'][name_property]))
+                else:
+                    print(entity['metadata']['guid'])
+        elif arguments.action.find('get_') == 0:
+            domain = arguments.action[len('get_'):]
+            resource_id = resolve_id(arguments.id[0],
+                                     lambda x: _get_client_domain(client, domain).get_first(
+                                         **{commands[domain]['name']: x}),
+                                     domain,
+                                     commands[domain]['allow_retrieve_by_name'])
+            print(_get_client_domain(client, domain).get(resource_id).json(indent=1))
+        elif arguments.action.find('create_') == 0:
+            domain = arguments.action[len('create_'):]
+            data = None
+            if os.path.isfile(arguments.entity[0]):
+                with open(arguments.entity[0], 'r') as f:
+                    try:
+                        data = json.load(f)
+                    except ValueError:
+                        raise ValueError('entity: file %s does not contain valid json data' % arguments.entity[0])
             else:
-                print(entity['metadata']['guid'])
-    elif arguments.action.find('get_') == 0:
-        domain = arguments.action[len('get_'):]
-        resource_id = resolve_id(arguments.id[0],
-                                 lambda x: _get_client_domain(client, domain).get_first(
-                                     **{commands[domain]['name']: x}),
-                                 domain,
-                                 commands[domain]['allow_retrieve_by_name'])
-        print(_get_client_domain(client, domain).get(resource_id).json(indent=1))
-    elif arguments.action.find('create_') == 0:
-        domain = arguments.action[len('create_'):]
-        data = None
-        if os.path.isfile(arguments.entity[0]):
-            with open(arguments.entity[0], 'r') as f:
                 try:
-                    data = json.load(f)
+                    data = json.loads(arguments.entity[0])
                 except ValueError:
-                    raise ValueError('entity: file %s does not contain valid json data' % arguments.entity[0])
-        else:
-            try:
-                data = json.loads(arguments.entity[0])
-            except ValueError:
-                raise ValueError('entity: must be either a valid json file path or a json object')
-        print(_get_client_domain(client, domain)._create(data).json())
-    elif arguments.action.find('delete_') == 0:
-        domain = arguments.action[len('delete_'):]
-        if is_guid(arguments.id[0]):
-            _get_client_domain(client, domain)._remove(arguments.id[0])
-        elif commands[domain]['allow_retrieve_by_name']:
-            filter_get = dict()
-            filter_get[commands[domain]['name']] = arguments.id[0]
-            entity = _get_client_domain(client, domain).get_first(**filter_get)
-            if entity is None:
-                raise InvalidStatusCode(NOT_FOUND, '%s with name %s' % (domain, arguments.id[0]))
+                    raise ValueError('entity: must be either a valid json file path or a json object')
+            print(_get_client_domain(client, domain)._create(data).json())
+        elif arguments.action.find('delete_') == 0:
+            domain = arguments.action[len('delete_'):]
+            if is_guid(arguments.id[0]):
+                _get_client_domain(client, domain)._remove(arguments.id[0])
+            elif commands[domain]['allow_retrieve_by_name']:
+                filter_get = dict()
+                filter_get[commands[domain]['name']] = arguments.id[0]
+                entity = _get_client_domain(client, domain).get_first(**filter_get)
+                if entity is None:
+                    raise InvalidStatusCode(NOT_FOUND, '%s with name %s' % (domain, arguments.id[0]))
+                else:
+                    _get_client_domain(client, domain)._remove(entity['metadata']['guid'])
             else:
-                _get_client_domain(client, domain)._remove(entity['metadata']['guid'])
-        else:
-            raise ValueError('id: %s: does not allow search by name' % domain)
+                raise ValueError('id: %s: does not allow search by name' % domain)
 
 
 if __name__ == "__main__":
