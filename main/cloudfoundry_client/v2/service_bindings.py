255dc2ebeb285b61ddbbee3d0f92d07bb50c61fc
Fouad Hamdi <1621862+fouadh@users.noreply.github.com>
2019-01-27 14:23:56 +0100

    Give the possibility to specify a binding name at creation

--- main/cloudfoundry_client/v2/service_bindings.py
+++ main/cloudfoundry_client/v2/service_bindings.py
@@ -5,9 +5,10 @@ class ServiceBindingManager(EntityManager):
     def __init__(self, target_endpoint, client):
         super(ServiceBindingManager, self).__init__(target_endpoint, client, '/v2/service_bindings')
 
-    def create(self, app_guid, instance_guid, parameters=None):
+    def create(self, app_guid, instance_guid, parameters=None, binding_name=None):
         request = dict(app_guid=app_guid,
-                       service_instance_guid=instance_guid)
+                       service_instance_guid=instance_guid,
+                       name=binding_name)
         if parameters is None:
             request['parameters'] = {}
         else:
