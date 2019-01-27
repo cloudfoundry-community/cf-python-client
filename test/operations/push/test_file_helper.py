f5d341e7499bb2cd02dff53d4dd3e79e4ee00c17
Fouad Hamdi <1621862+fouadh@users.noreply.github.com>
2019-01-27 14:10:32 +0100

    Fix unzip method to make it Python 2 compatible

---
+++ test/operations/push/test_file_helper.py
@@ -0,0 +1,46 @@
+import os
+import shutil
+import tempfile
+import zipfile
+from unittest import TestCase
+
+from cloudfoundry_client.operations.push.file_helper import FileHelper
+
+
+class TestUnzipHelper(TestCase):
+    def setUp(self):
+        self.input_dirpath = self.zip_some_data()
+        self.output_dirpath = tempfile.mkdtemp()
+
+    def test_unzip(self):
+        self.unzip()
+        self.assertFileUnzipped()
+
+    def test_unzip_with_existing_output_subdir(self):
+        os.makedirs(self.output_dirpath + '/some_dir/subdir')
+        self.unzip()
+
+        self.assertFileUnzipped()
+
+    def unzip(self):
+        FileHelper.unzip(self.input_dirpath + '/myzip.zip', self.output_dirpath)
+
+    def zip_some_data(self):
+        input_dirpath = self.prepare_data_to_zip()
+        with zipfile.ZipFile(input_dirpath + '/myzip.zip', 'w', zipfile.ZIP_DEFLATED) as myzip:
+            myzip.write(input_dirpath + '/file.txt', 'file.txt')
+            myzip.write(input_dirpath + '/some_dir', 'some_dir')
+            myzip.write(input_dirpath + '/some_dir/subdir', 'some_dir/subdir')
+        return input_dirpath
+
+    def prepare_data_to_zip(self):
+        input_dirpath = tempfile.mkdtemp()
+        shutil.copyfile(__file__, input_dirpath + '/file.txt')
+        os.makedirs(input_dirpath + '/some_dir/subdir')
+        shutil.copyfile(__file__, input_dirpath + '/some_dir/file.txt')
+        return input_dirpath
+
+    def assertFileUnzipped(self):
+        self.assertTrue(os.path.isfile(self.output_dirpath + '/file.txt'))
+        self.assertTrue(os.path.isdir(self.output_dirpath + '/some_dir'))
+        self.assertTrue(os.path.isdir(self.output_dirpath + '/some_dir/subdir'))
