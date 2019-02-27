fc2602746053f798882ef11e9b00cd4fa4b251e8
Abhishek <abhi.log@gmail.com>
2019-02-27 09:51:41 -0600

    fixed bug for user manager

--- main/cloudfoundry_client/v2/users.py
+++ main/cloudfoundry_client/v2/users.py
@@ -3,8 +3,4 @@ from cloudfoundry_client.json_object import JsonObject
 
 class UserManager(object):
     def __init__(self, target_endpoint, client):
-        self.target_endpoint = target_endpoint
-        self.client = client
-
-    def get(self, job_guid):
-        return self.client.get('%s/v2/users/%s' % (self.target_endpoint, job_guid)).json(object_pairs_hook=JsonObject)
+        super(UserManager, self).__init__(target_endpoint, client, '/v2/users')
