441881b0f4cee20ad399f50773b83025c5fa30f7
Cooper <onkelcoop@gmail.com>
2019-11-18 10:12:47 +0100

    add some types

--- main/cloudfoundry_client/v3/domains.py
+++ main/cloudfoundry_client/v3/domains.py
@@ -5,8 +5,9 @@ class DomainManager(EntityManager):
     def __init__(self, target_endpoint, client):
         super(DomainManager, self).__init__(target_endpoint, client, '/v3/domains')
 
-    def create(self, name, internal=False, organization=None, shared_organizations=None,
-               meta_labels=None, meta_annotations=None):
+    def create(self, name: str, internal: bool = False, organization: str = None,
+               shared_organizations: str = None, meta_labels: dict = None,
+               meta_annotations: dict = None) -> 'Entity':
         data = {
             'name': name,
             'internal': internal,
@@ -19,11 +20,12 @@ class DomainManager(EntityManager):
         }
         return super(DomainManager, self)._create(data)
 
-    def list_domains_for_org(self, org_guid, **kwargs):
+    def list_domains_for_org(self, org_guid: str, **kwargs):
         uri = '/v3/organizations/{guid}/domains'.format(guid=org_guid)
         return self._list(uri, **kwargs)
 
-    def update(self, domain_guid, meta_labels=None, meta_annotations=None):
+    def update(self, domain_guid: str, meta_labels: dict = None,
+               meta_annotations: dict = None) -> 'Entity':
         data = {
             'metadata': {
                 'labels': meta_labels,
@@ -32,16 +34,16 @@ class DomainManager(EntityManager):
         }
         return super(DomainManager, self)._update(domain_guid, data)
 
-    def remove(self, domain_guid):
+    def remove(self, domain_guid: str):
         super(DomainManager, self)._remove(domain_guid)
 
-    def __create_shared_domain_url(self, domain_guid):
+    def __create_shared_domain_url(self, domain_guid) -> str:
         # TODO use url parser for this
         return '{endpoint}{entity}/{domain}/relationships/shared_organizations' \
                ''.format(endpoint=self.target_endpoint, entity=self.entity_uri,
                          domain=domain_guid)
 
-    def share_domain(self, domain_guid, organization_guids):
+    def share_domain(self, domain_guid: str, organization_guids: str) -> 'Entity':
         if type(organization_guids) is not list():
             organization_guids = [organization_guids]
         url = self.__create_shared_domain_url(domain_guid)
@@ -50,7 +52,7 @@ class DomainManager(EntityManager):
             data.append({'guid': org})
         return super(DomainManager, self)._post(url, data=data)
 
-    def unshare_domain(self, domain_guid, org_guid):
+    def unshare_domain(self, domain_guid: str, org_guid: str):
         url = '{uri}/{org}'.format(uri=self.__create_shared_domain_url(domain_guid),
                                    org=org_guid)
         super(DomainManager, self)._delete(url)
