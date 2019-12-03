badd280f744e91afc519753e72d8e1ae87ece691
Cooper <onkelcoop@gmail.com>
2019-12-03 14:58:53 +0100

    [network-policy] add delete test

--- test/networking/v1/external/test_policies.py
+++ test/networking/v1/external/test_policies.py
@@ -2,7 +2,7 @@ import unittest
 from http import HTTPStatus
 
 from abstract_test_case import AbstractTestCase
-from cloudfoundry_client.networking.v1.external.policies import PolicyManager
+from cloudfoundry_client.networking.v1.external.policies import Policy
 from fake_requests import mock_response
 
 
@@ -24,3 +24,23 @@ class TestApps(unittest.TestCase, AbstractTestCase):
         self.assertEqual(2, len(all_policies))
         self.assertEqual(all_policies[0]['source']['id'], "1081ceac-f5c4-47a8-95e8-88e1e302efb5")
         self.assertEqual(all_policies[0]['destination']['id'], "38f08df0-19df-4439-b4e9-61096d4301ea")
+
+    def test_delete(self):
+        self.client.delete.return_value = mock_response('/networking/v1/external/policies/delete',
+                                                        HTTPStatus.OK,
+                                                        None)
+        policy = Policy(src_id='1081ceac-f5c4-47a8-95e8-88e1e302efb5',
+                        dst_id='38f08df0-19df-4439-b4e9-61096d4301ea',
+                        proto='tcp',
+                        start_port=1234,
+                        end_port=1234)
+        self.client.networking_v1_external.policies.delete([policy, ])
+        self.client.delete.assert_called_with({'policies': [
+                                                  {'source':
+                                                       {'id': '1081ceac-f5c4-47a8-95e8-88e1e302efb5'},
+                                                   'destination': {
+                                                       'id': '38f08df0-19df-4439-b4e9-61096d4301ea',
+                                                       'ports': {'start': 1234, 'end': 1234},
+                                                       'protocol': 'tcp'}
+                                                   }
+                                              ]})
