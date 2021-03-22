fef8525343034190a7fc42827f2a4c679040c449
Jakob Schmid <jakob.schmid@sap.com>
2021-03-22 17:14:00 +0100

    Add tests for synchronous SI deletion

--- test/v3/test_service_instances.py
+++ test/v3/test_service_instances.py
@@ -1,6 +1,6 @@
 import unittest
 from http import HTTPStatus
-
+from unittest.mock import patch
 from abstract_test_case import AbstractTestCase
 from cloudfoundry_client.v3.entities import Entity
 
@@ -31,3 +31,38 @@ class TestServiceInstances(unittest.TestCase, AbstractTestCase):
         self.client.get.assert_called_with(self.client.get.return_value.url)
         self.assertEqual("85ccdcad-d725-4109-bca4-fd6ba062b5c8", service_instance["guid"])
         self.assertIsInstance(service_instance, Entity)
+
+    def test_remove_user_provided_service_instance(self):
+        self.client.delete.return_value = self.mock_response("/v3/service_instances/service_instance_id", HTTPStatus.NO_CONTENT, None)
+        self.client.v3.service_instances.remove("service_instance_id")
+        self.client.delete.assert_called_with(self.client.delete.return_value.url)
+
+    def test_remove(self):
+        self.client.delete.return_value = self.mock_response("/v3/service_instances/service_instance_id", HTTPStatus.ACCEPTED, headers={"Location": "https://somewhere.org/v3/jobs/job_id"})
+        self.client.v3.service_instances.remove("service_instance_id")
+        self.client.delete.assert_called_with(self.client.delete.return_value.url)
+
+    @patch("time.sleep", return_value=None)
+    def test_remove_synchronous(self, sleepmock):
+        self.client.delete.return_value = self.mock_response("/v3/service_instances/service_instance_id", HTTPStatus.ACCEPTED, {"Location": "https://somewhere.org/v3/jobs/job_id"})
+        self.client.get.side_effect = [
+            self.mock_response(
+                "/v3/jobs/job_id",
+                HTTPStatus.OK,
+                None,
+                "v3",
+                "jobs",
+                "GET_{id}_processing_response.json",
+            ),
+            self.mock_response(
+                "/v3/jobs/job_id",
+                HTTPStatus.OK,
+                None,
+                "v3",
+                "jobs",
+                "GET_{id}_complete_response.json",
+            ),
+        ]
+        self.client.v3.service_instances.remove("service_instance_id",asynchronous=False)
+        self.client.delete.assert_called_with(self.client.delete.return_value.url)
+        assert self.client.get.call_count == 2
