67b91ea38e301878720b007985019008851b4cc1
alex wang <alex.wang@autozone.com>
2021-05-05 12:59:05 -0500

    add get current droplet per app method

--- main/cloudfoundry_client/v3/apps.py
+++ main/cloudfoundry_client/v3/apps.py
@@ -32,3 +32,6 @@ class AppManager(EntityManager):
 
     def get_routes(self, application_guid: str) -> JsonObject:
         return super(AppManager, self)._get("%s%s/%s/routes" % (self.target_endpoint, self.entity_uri, application_guid))
+
+    def get_current_droplet(self, application_guid: str) -> JsonObject:
+        return super(AppManager, self)._get("%s%s/%s/droplets/current" % (self.target_endpoint, self.entity_uri, application_guid))
