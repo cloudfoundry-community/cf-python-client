be3c0d1122490313182d23b79ebbbc72a885cca2
Brian Pandola <bpandola@gmail.com>
2024-01-02 17:11:07 -0800

    Drop support for Python 3.7

--- setup.py
+++ setup.py
@@ -69,12 +69,12 @@ setup(
     description="A client library for CloudFoundry",
     long_description=open("README.rst").read(),
     url="http://github.com/antechrestos/cf-python-client",
+    python_requires=">=3.8",
     classifiers=[
         "Programming Language :: Python",
         "Natural Language :: English",
         "Operating System :: OS Independent",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
