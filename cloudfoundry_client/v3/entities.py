6472c32cdd4b3300353ec1d2e74d1225f9b06dd1
Krzysztof Marciniak <Krzysztof_Marciniak@swissre.com>
2025-07-30 12:46:04 +0200

    tests + removed additional quotation in entities.py

--- cloudfoundry_client/v3/entities.py
+++ cloudfoundry_client/v3/entities.py
@@ -302,7 +302,7 @@ class EntityManager(object):
                 parameters.append("%s=%s" % (parameter_name, quote(",".join(parameter_value))))
             elif isinstance(parameter_value, (dict)) and parameter_name == "fields":
                 for resource, key in parameter_value.items():
-                    parameters.append("%s[%s]=%s" % (parameter_name, resource, quote(",".join(key))))
+                    parameters.append("%s[%s]=%s" % (parameter_name, resource, ",".join(key)))
             else:
                 parameters.append("%s=%s" % (parameter_name, quote(str(parameter_value))))
             return parameters
